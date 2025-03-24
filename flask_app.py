f3c17f78e06de22684833f2bad43b595115a7166
SamuraiZach <81524537+SamuraiZach@users.noreply.github.com>
2025-03-23 20:33:45 -0400

    basics

--- flask_app.py
+++ flask_app.py
@@ -1,8 +1,18 @@
-# A very simple Flask Hello World app for you to get started with...
+import os
+from flask import Flask, render_template, request
+from openai import OpenAI
+from dotenv import load_dotenv
 
-from flask import Flask
-
+load_dotenv()
 app = Flask(__name__)
+
+token = os.getenv["GITHUB_TOKEN"]
+endpoint = "https://models.inference.ai.azure.com";
+modelName = "gpt-4o";
+client = OpenAI(
+    base_url=endpoint,
+    api_key=token,
+)
 
 @app.route('/')
 def hello_world():
